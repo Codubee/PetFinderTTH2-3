474eb0ec894c618e1fba6315e17367f68563201b
Ashleigh Trout <ajt190001@utdallas.edu>
2021-03-18 20:59:31 -0500

    Filled Explanation.js and added component to the landing page.

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,1 +1,21 @@
 // This is where the explanation component will be created
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+
+function Explanation() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1 className="display-4">Pet Finder</h1>
+        <p className="lead">A mobile app that matches potential pet owners to shelter animals in need of a home.</p>
+        <hr className="my-4" />
+        <p>
+          This project was created during the Spring 2021 session of Codubee by the T/TH 2-3 group. The app hopes to help shelter animals 
+          connect with potential owners by letting users swipe left/right based on their preferences. 
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Explanation;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Explanation from '../components/Explanation.js'; 
 
 function LandingPage(){
     return(
         <div>
             {/* Insert your created components here */}
             <h1>Landing Page Component</h1>
+
+            {/* Explanation Component*/}
+            <Explanation/>
         </div>
     )
 }
