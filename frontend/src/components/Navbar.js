4e059bc70ee0c7daf593574c5851d2f62793e3e0
ShivaniRGandhi <shivanig1025@gmail.com>
2021-03-25 15:01:52 -0500

    Created the navbar with links to landing page and matches

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,58 +1,20 @@
-
-import React, { useState } from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
 } from 'reactstrap';
 
-const Example = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
+function navbar() {
   return (
     <div>
-      <Navbar color='info' light expand="md">
-        <NavbarBrand href="/">Home</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Matches</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Pet Finder</NavbarText>
-        </Collapse>
+      <Navbar className="color-nav">
+        <Nav>
+        <NavbarBrand className ="color-text" href="./views/LandingPage.js">Pet Finder</NavbarBrand>
+        <NavLink className="color-text" href="/">Matches</NavLink>
+        </Nav>
       </Navbar>
     </div>
   );
 }
-
-export default Example;
+export default navbar;
