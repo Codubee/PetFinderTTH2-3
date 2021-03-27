7770230f54594ae88e9f68dacd66324a43ccae45
Aashritha <aashrithaa2011@gmail.com>
2021-03-26 22:27:30 -0500

    Tools-component

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Tools from '../components/Tools.js'
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
+            <Tools/>
             <h1>Landing Page Component</h1>
         </div>
     )
