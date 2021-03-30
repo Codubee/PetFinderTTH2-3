1a15d7ffe0ac1336b67ffe5bbc1e42c3850d8647
HannahMakenzie <77458466+HannahMakenzie@users.noreply.github.com>
2021-03-30 16:07:36 -0500

    removed landing page text

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -5,7 +5,6 @@ function LandingPage(){
     return(
         <div>
             {/* Insert your created components here */}
-            <h1>Landing Page Component</h1>
             <TeamCards/>
         </div>
     )
