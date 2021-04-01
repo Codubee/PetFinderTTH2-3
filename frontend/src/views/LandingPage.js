b229c0d45fba6a090343794e46d8f6898a7699f7
Brie Haynes <brieyananicole@gmail.com>
2021-04-01 12:31:54 -0500

    Removed Landing Page component

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Features from '../components/Features';
+import Features from '../components/Features.js'
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
-            <h1>Landing Page Component</h1>
-
             <Features/>
         </div>
     )
