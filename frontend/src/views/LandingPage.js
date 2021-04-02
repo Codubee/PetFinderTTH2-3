a2953c79ca31dc6f71442f855fac67af63953731
Brie <55420322+brieyana@users.noreply.github.com>
2021-04-02 16:23:53 -0500

    TTh-3-Features (#3)
    * Created features component
    
    * Created styling for features component
    
    * Imported features component
    
    * Deleted 'Features' styling
    
    * Imported Features.css
    
    * Added styles folder
    
    * Removed Landing Page component
    
    * Moved center css to app.css, renamed the container to the features container
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Features from '../components/Features.js'
 import Team from '../components/Team.js'
 
 function LandingPage(){
     return(
         <div>
+            <Features/>
             <Team/>
         </div>
     )
