74346a2f3fdc12050880a5fbc80e9d7415682266
ShivaniRGandhi <shivanig1025@gmail.com>
2021-04-08 21:59:46 -0500

    created a component that displays an animal given a prop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
 import Navbar from './components/Navbar.js'
+import DisplayAnimal from './components/DisplayAnimal.js'
 
 function App() {
   return (
     <div>
       <Navbar />
       <LandingPage />
+      <DisplayAnimal image='https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg'/>
     </div>
   );
 };
