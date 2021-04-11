076957d1d2aea810e057b02b08410e9107606fe1
codubee <info@codubee.com>
2021-04-11 18:09:36 -0500

    Removed animal description from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
 import Navbar from './components/Navbar.js'
-import AnimalDescription from './components/AnimalDescription.js'
 
 function App() {
   return (
     <div>
       <Navbar />
       <LandingPage/>
-      <div className="Description">
-        <AnimalDescription name="ruff" type="dog" sex="female" shelter_name="shelter"
-            adoption_url="example.com"></AnimalDescription>
-      </div>
     </div>
   );
 };
