9142f6ee12b9758f9ee487ef85101d31e6c3d6fe
codubee <info@codubee.com>
2021-04-11 18:13:20 -0500

    Changed named of the display animal function

--- frontend/src/components/DisplayAnimal.js
+++ frontend/src/components/DisplayAnimal.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 /* this function takes a prop as a parameter which will be the image url */
 
-function ShowImage(props) {
+function DisplayAnimal(props) {
     return <img src={props.image} />;
 }
 
-export default ShowImage;
+export default DisplayAnimal;
