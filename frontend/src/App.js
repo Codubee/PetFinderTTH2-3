9953822d52edc58388af6a24cf5624f0880c72c8
HannahMakenzie <77458466+HannahMakenzie@users.noreply.github.com>
2021-04-11 15:04:13 -0500

    added the component to display animal info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
 import Navbar from './components/Navbar.js'
+import AnimalDescription from './components/AnimalDescription.js'
 
 function App() {
   return (
     <div>
       <Navbar />
       <LandingPage/>
+      <div className="Description">
+        <AnimalDescription name="ruff" type="dog" sex="female" shelter_name="shelter"
+            adoption_url="example.com"></AnimalDescription>
+      </div>
     </div>
   );
 };
