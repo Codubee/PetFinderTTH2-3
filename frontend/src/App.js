8c0076f10e7a2b31c10d6169f04310a43120d129
Brie Haynes <brieyananicole@gmail.com>
2021-04-22 09:11:09 -0500

    Implemented routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import LandingPage from './views/LandingPage'
-import Navbar from './components/Navbar.js'
+import LandingPage from './views/LandingPage';
+import Navbar from './components/Navbar.js';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <LandingPage />
+      <div>
+          <Navbar />
+
+          <Router>
+            <Switch>
+              <Route path="/">
+                <LandingPage />
+              </Route>
+            </Switch>
+          </Router>
+
+      </div>
     </div>
   );
+
+
 };
 
 export default App;
