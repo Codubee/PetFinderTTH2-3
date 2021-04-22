d32eeaa8a7c502690e276f00bb2da3d7cb554713
aashritha2001 <aashrithaa2011@gmail.com>
2021-04-22 18:32:38 -0500

    added animals array, table now displays multiple animals

--- frontend/src/components/DisplayMatches.js
+++ frontend/src/components/DisplayMatches.js
@@ -3,28 +3,41 @@ import { Table } from 'reactstrap';
 
 /* This function displays the matches in a bordered table  */
 const DisplayMatches = (props) => {
+
+  /* Add animals to this array to display matches */
+  const animals = [
+    { name: "Leo", type: "Dog", sex: "Male", shelterName: "Shelter_name", adoptionUrl: "example.com" },
+    { name: "Martha", type: "Cat", sex: "Female", shelterName: "Shelter-name2", adoptionUrl: "example2.com" },
+    { name: "Dory", type: "Fish", sex: "Female", shelterName: "Shelter-name2", adoptionUrl: "example3.com" }
+  ]
+
   return (
     <Table bordered>
-      <thead>
-        <tr>
+      <div>
+        <thead>
+          <tr>
             <th></th>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Sex</th>
-          <th>Shelter Name</th>
-          <th>Adoption Url</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Leo</td>
-          <td>Dog</td>
-          <td>Female</td>
-          <td>Shelter</td>
-          <td>example.com</td>
-        </tr>
-      </tbody>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Sex</th>
+            <th>Shelter Name</th>
+            <th>Adoption Url</th>
+          </tr>
+        </thead>
+        {animals.map((animals, id) => (
+          <tbody key={id}>
+            <tr>
+              <th scope="row">{id + 1}</th>
+              <td>{animals.name}</td>
+              <td>{animals.type}</td>
+              <td>{animals.sex}</td>
+              <td>{animals.shelterName}</td>
+              <td><a href={animals.adoptionUrl}> {animals.adoptionUrl} </a></td>
+            </tr>
+          </tbody>
+        ))
+        }
+      </div>
     </Table>
   );
 }
