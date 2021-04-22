c81c0b90a767fd2d3011559e81b7b89e0a200519
Brie Haynes <brieyananicole@gmail.com>
2021-04-22 09:13:11 -0500

    Accepted incoming changes

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,24 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import Features from '../components/Features.js'
-=======
 import Tools from '../components/Tools.js'
 import Explanation from '../components/Explanation.js'; 
 import Features from '../components/Features.js'
 import Team from '../components/Team.js'
->>>>>>> main
 
 function LandingPage(){
     return(
         <div>
-<<<<<<< HEAD
-            <Features/>
-=======
             <Explanation/>
             <Tools/>
             <Features/>
             <Team/>
->>>>>>> main
         </div>
     )
 }
