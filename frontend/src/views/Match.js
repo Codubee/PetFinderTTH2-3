82a90b622822e996274e35f13d458934d0d7759d
aashritha2001 <75947163+aashritha2001@users.noreply.github.com>
2021-04-28 21:55:45 -0500

    TTh-12-Display Matches (#10)
    * Display Matched compoment
    
    * added comments
    
    * added animals array, table now displays multiple animals
    
    * created style folder
    
    * added color to table
    
    * Fixed table spacing, added collapse
    
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Container } from 'reactstrap'
+import React,{useState} from 'react'
+import { Container,Collapse,Button } from 'reactstrap'
 import DisplayAnimal from '../components/DisplayAnimal'
 import AnimalDescription from '../components/AnimalDescription'
 import '../styles/Match.css'
+import DisplayMatches from '../components/DisplayMatches.js';
 
 function Match(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+  
     return(
         <Container className="text-center pt-md">
             <DisplayAnimal/>
@@ -15,6 +20,12 @@ function Match(){
             <button className="NoButton">
                 No
             </button>
+            <div>
+                <Button color="info" className="collapse-button" onClick={toggle} >Matches</Button>
+                <Collapse isOpen={isOpen}>
+                    <DisplayMatches/>
+                </Collapse>
+            </div>
         </Container>
     )
 }
