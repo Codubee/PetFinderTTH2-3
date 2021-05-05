28158a60d74e785f4fde9970085211517ef61846
codubee <info@codubee.com>
2021-05-04 23:47:20 -0500

    Added the proxy, change match to class component

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,15 +1,23 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { Container,Collapse,Button } from 'reactstrap'
 import DisplayAnimal from '../components/DisplayAnimal'
 import AnimalDescription from '../components/AnimalDescription'
 import '../styles/Match.css'
 import DisplayMatches from '../components/DisplayMatches.js';
 
-function Match(){
-    const [isOpen, setIsOpen] = useState(false);
+class Match extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = { isOpen: false }
+        this.setIsOpen = this.setIsOpen.bind(this)
+    }
 
-    const toggle = () => setIsOpen(!isOpen);
-  
+    setIsOpen() {
+        var open = !this.state.isOpen;
+        this.setState({ isOpen: open })
+    }
+
+    render(){
     return(
         <Container className="text-center pt-md">
             <DisplayAnimal/>
@@ -21,13 +29,14 @@ function Match(){
                 No
             </button>
             <div>
-                <Button color="info" className="collapse-button" onClick={toggle} >Matches</Button>
-                <Collapse isOpen={isOpen}>
+                <Button color="info" className="collapse-button" onClick={this.setIsOpen} >Matches</Button>
+                <Collapse isOpen={this.state.isOpen}>
                     <DisplayMatches/>
                 </Collapse>
             </div>
         </Container>
     )
+}
 }
 
 export default Match;
