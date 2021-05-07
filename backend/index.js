4f0d45995bcda688afe4ba5bdd502a0570afecc6
ShivaniRGandhi <56098361+ShivaniRGandhi@users.noreply.github.com>
2021-05-07 14:43:47 -0500

    [TTH-18]added the /addAnimal route to the index.js file (#13)
    * added the /addAnimal route to the index.js file
    
    * fixed the addAnimal route to include the body as input
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,20 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
+/* function which receives the animal data in the body or postman and adds it to a specific user */
+app.post('/addAnimal', function (req, res) {
+    axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal', req.body) // use axios to call /addAnimal
+        .then(function (response) {
+            // handle success and send back a 200 response with the data
+            console.log(response.data);
+            res.status(200).json(response.data);
+        })
+        .catch(function (error) {
+            // handle the error and send back the error message that was received from the API call
+            console.log(error)
+            res.status(400).json({ error: "An error occurred" });
+        })
+})
 
 /* Function that will return a user's matches */
 app.get('/getMatches', function (req, res) {
