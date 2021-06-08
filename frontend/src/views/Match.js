46abe1181ef97ee6e002fd66df24abbe5031a900
codubee <info@codubee.com>
2021-06-08 15:50:46 -0700

    GChanged get descriptions to a function

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,73 +1,73 @@
 import React from 'react'
-import axios from 'axios'
-import { Container,Collapse,Button } from 'reactstrap'
+import { Container, Collapse, Button } from 'reactstrap'
 import DisplayAnimal from '../components/DisplayAnimal'
 import AnimalDescription from '../components/AnimalDescription'
 import '../styles/Match.css'
 import DisplayMatches from '../components/DisplayMatches.js';
 import axios from 'axios'
 
-class Match extends React.Component{
+class Match extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { isOpen: false, data:{}}
+        this.state = { isOpen: false, data: {}, id: 0 }
         this.setIsOpen = this.setIsOpen.bind(this)
         this.addAnimal = this.addAnimal.bind(this);
+        this.getDescription = this.getDescription.bind(this)
     }
+
+    componentDidMount() {
+        this.getDescription();
+        this.setState({ id: Math.floor((Math.random() * 1000) + 1) })
+    }
+
     addAnimal() {
         var body = {
-            "id":1,
-            "record": {
-                "name": "Muffin",
-                "type": "Shepherd / Mix",
-                "sex": "Male",
-                "shelter_name": "Austin Pets Alive",
-                "adoption_url": "https://www.austinpetsalive.org/adopt/dogs/apa-a-56615",
-                "image": "https://www.austinpetsalive.org/assets/animals/_largeThumb/20190606003943.jpeg"
-            }
+            "id": this.state.id,
+            "record": this.state.data
         }
+
         axios.post('/addAnimal', body)
-             .then((response) => {
+            .then((response) => {
                 console.log(response.data);
+                this.getDescription();
             });
     }
 
-    componentDidMount(){
+    getDescription() {
         axios.get('/getAnimalDescription')
-        .then((response) =>{
-            this.setState({
-                data:response.data
+            .then((response) => {
+                this.setState({
+                    data: response.data
+                })
             })
-        })
     }
-
     setIsOpen() {
         var open = !this.state.isOpen;
         this.setState({ isOpen: open })
     }
 
-    render(){
-    return(
-        <Container className="text-center pt-md">
+    render() {
+        return (
+            <Container className="text-center pt-md">
 
-            <DisplayAnimal image={this.state.data.image}/>
-            <AnimalDescription animal={this.state.data} />
-            <button onClick={this.addAnimal} className="YesButton">
+                <DisplayAnimal image={this.state.data.image} />
+                <AnimalDescription animal={this.state.data} />
+                <button onClick={this.addAnimal} className="YesButton">
 
-                Yes
+                    Yes
             </button>
-            <button className="NoButton">
-                No
-            </button>
-            <div>
-                <Button color="info" className="collapse-button" onClick={this.setIsOpen} >Matches</Button>
-                <Collapse isOpen={this.state.isOpen}>
-                    <DisplayMatches/>
-                </Collapse>
-            </div>
-        </Container>
-    )
-}
+                <button className="NoButton" onClick={this.getDescription}>
+                    No
+                </button>
+                <div>
+                    <Button color="info" className="collapse-button" onClick={this.setIsOpen} >Matches</Button>
+                    <Collapse isOpen={this.state.isOpen}>
+                        <DisplayMatches />
+                    </Collapse>
+                </div>
+            </Container>
+        )
+    }
 }
 
 export default Match;
